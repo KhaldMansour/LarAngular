99654472b8c83c12ad0bf0a94bef756de7b49ef7
khaldmansour <khaldmansour93@gmail.com>
2020-10-19 05:22:23 +0200

    Resoving Conflicts

--- Front/Frontend/src/app/components/password/request-resend/request-resend.component.ts
+++ Front/Frontend/src/app/components/password/request-resend/request-resend.component.ts
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 import { AuthService } from './../../../Services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
-=======
-import { SnotifyService } from 'ng-snotify';
-import { AuthService } from './../../../Services/auth.service';
-import { Component, OnInit } from '@angular/core';
->>>>>>> e3595c2a660e996fa94efbbe3718dfaf79a3964c
 
 @Component({
   selector: 'app-request-resend',
